028ac9c121fcaeedaaf821f91a63ffd1b040bbf7
mohamedamid <amid.mohamed2024@gmail.com>
2024-12-09 16:06:18 +0100

    create table users

--- Sql.sql
+++ Sql.sql
@@ -12,3 +12,15 @@ CREATE TABLE subscription (
     MonthlyFree DECIMAL(10,2)
 );
 
+--CREATE TABLE users
+
+CREATE TABLE users (
+    userId INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    RegistrationDate DATE NOT NULL,
+    SubscriptionID INT,
+    FOREIGN KEY (SubscriptionID) REFERENCES subscription(SubscriptionID)
+);
+
