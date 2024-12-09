e3f8e6edf4c42aca20545813ef00f6414d0930f8
mohamedamid <amid.mohamed2024@gmail.com>
2024-12-09 16:00:40 +0100

    create table subscription

--- Sql.sql
+++ Sql.sql
@@ -4,3 +4,11 @@ CREATE DATABASE stream;
 
 USE stream;
 
+--CREATE TABLE subscription
+
+CREATE TABLE subscription (
+    SubscriptionID INT PRIMARY KEY AUTO_INCREMENT,
+    SubscriptionType VARCHAR(50) NOT NULL,
+    MonthlyFree DECIMAL(10,2)
+);
+
